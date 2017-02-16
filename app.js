73079800e3ef81d236428aed04c13ecee178f973
Derek Houck <derek@goldman-marketing.com>
2017-02-15 17:46:16 -0800

    Implement basic Facebook SDK

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/index.htm', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.htm" );
 })
 
